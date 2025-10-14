353e761d2905cd784872ddc94bbde79b661523eb
xiaosongpi <minixiaopipi@gmail.com>
2025-10-14 19:16:47 +0700

    Applying backdrop for better UI for navigation bar

--- my-app/src/app/components/Navbar.jsx
+++ my-app/src/app/components/Navbar.jsx
@@ -15,13 +15,13 @@ export default function Navbar () {
     return (
         <nav className="w-full max-w-[1440] px-12 pt-5 fixed top-0 left-1/2 -translate-x-1/2">
             <div className="relative flex">
-                <div className="h-12 px-5 bg-gradient-to-tl from-[#00000080] via-[#56565640] to-[#d0d0d080] flex flex-row items-center justify-center gap-2 border rounded-full cursor-pointer hover:shadow-[0_0_15px_#ffffff] transition duration-500">
+                <div className="h-12 px-5 bg-gradient-to-tl from-[#00000080] via-[#56565640] to-[#d0d0d080] backdrop-blur-lg flex flex-row items-center justify-center gap-2 border rounded-full cursor-pointer hover:shadow-[0_0_15px_#ffffff] transition duration-500">
                     <div className="w-1.5 h-1.5 bg-emerald-400 rounded-full" />
                     <p className="">Anonymous</p>
                 </div>
                 <div className="absolute right-0 grid grid-rows-[repeat(2, auto)] border rounded-full">
                     <button 
-                        className="w-12 h-12 bg-linear-to-tl from-[#00000080] via-[#56565640] to-[#d0d0d080] flex items-center justify-center border rounded-full cursor-pointer hover:shadow-[0_0_15px_#ffffff] transition duration-500"
+                        className="w-12 h-12 bg-linear-to-tl from-[#00000080] via-[#56565640] to-[#d0d0d080] backdrop-blur-lg flex flex items-center justify-center border rounded-full cursor-pointer hover:shadow-[0_0_15px_#ffffff] transition duration-500"
                         onClick={() => setMenuOpen(!openMenu)}
                     >
                         <Image 
