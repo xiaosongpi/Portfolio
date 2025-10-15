d887c677cc655c6549a112c1ee2e83bf67ea0aa3
xiaosongpi <minixiaopipi@gmail.com>
2025-10-15 16:22:30 +0700

    Creating project section

---
+++ my-app/src/app/components/Project.jsx
@@ -0,0 +1,63 @@
+export default function Project() {
+    return(
+        <div className="h-auto max-w-[1440] py-24 mx-auto px-12 flex items-center justify-center">
+            <div className="w-[90%] grid grid-cols-[40%_60%] gap-10">
+                <div className="flex flex-col justify-around">
+                    <div>
+                        <p className="text-9xl text-[#a5a5a5]"><span className="text-[#ffffff] font-bold">What</span> <br/>I Do</p>
+                        <p className="text-2xl">Let's check it out together, what project did I do after joining IT</p>
+                    </div>
+                    <div>
+                        <p className="text-9xl text-[#a5a5a5]"><span className="text-[#ffffff] font-bold">Know</span> <br/>More</p>
+                        <p className="text-2xl">Click the box for knowing more detail</p>
+                    </div>
+                </div>
+                <div className="h-100vh bg-[#131313] rounded-2xl">
+                    <div className="h-[50] px-8 bg-[#333333] rounded-t-2xl rounded-tr-2xl flex items-center gap-2">
+                        <div className="w-5 h-5 bg-[#EC6A63] rounded-full"></div>
+                        <div className="w-5 h-5 bg-[#F5BD52] rounded-full"></div>
+                        <div className="w-5 h-5 bg-[#5BC55A] rounded-full"></div>
+                    </div>
+                    <div className="p-5 grid grid-cols-2 gap-5">
+                        <div className="w-full h-fill min-h-40 bg-[#aaaaaa] row-span-2 rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 bg-[#aaaaaa] rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 bg-[#aaaaaa] rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 bg-[#aaaaaa] row-span-2 rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 bg-[#aaaaaa] row-span-2 rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 bg-[#aaaaaa] rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 bg-[#aaaaaa] rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 bg-[#aaaaaa] row-span-2 rounded-2xl">
+
+                        </div>
+                        <div className="w-full h-fill min-h-40 rounded-2xl">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Project from "./components/Project";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Project />
     </>
     // <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
     //   <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
