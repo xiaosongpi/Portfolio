86480ddc0bd3be70a2006b2cae4f0ded67c323df
xiaosongpi <minixiaopipi@gmail.com>
2025-10-24 10:33:46 +0700

    Rebuild hero section

--- my-app/src/app/components/Hero.jsx
+++ my-app/src/app/components/Hero.jsx
@@ -1,27 +1,25 @@
 export default function Hero() {
     return(
-        <div className="w-full max-w-[1440px] px-12 h-screen mx-auto flex items-center justify-center flex-col">
-            <div className="w-[90%]">
-                <p className="text-9xl text-[#a5a5a5]"><span className="font-bold text-white">My </span>Own</p>
-                <p className="text-9xl text-[#a5a5a5]">Portfolio <span className="font-bold text-white">Website</span></p>
-                <p className="mb-3 text-2xl">Know more about me, click here to download my CV</p>
-                <div className="w-fit p-1 bg-linear-to-br from-[#ffffff] to-[#333333] rounded-full">
-                    <button className="px-3 w-fit h-12 flex items-center justify-center bg-linear-to-br from-[#333333] to-[#000000] rounded-full gap-2.5 cursor-pointer">
-                        <div className="w-[40] h-[40] bg-[#ffffff] rounded-full flex items-center justify-center">
-                            <img 
-                                className="w-6 h-6" 
-                                src="/download.svg"
-                                alt="download" 
-                                width={24}
-                                height={24}
-                            />
+        <section className="mx-auto h-screen flex items-center justify-center">
+            <div className="flex flex-col">
+                <div className="flex items-center gap-5">
+                    <p className="text-6xl text-[#a5a5a5]">Welcome To</p>
+                    <div className="px-4 h-14 border border-[#a5a5a5] rounded-full shadow-2xl flex items-center gap-2.5 bg-[#e6e6e6]">
+                        <div className="h-2.5 w-2.5 rounded-full bg-green-500" />
+                        <p>Active looking for a job</p>
+                    </div>
+                </div>
+                <p className="mb-2.5 text-8xl">My Own Portfolio Webiste</p>
+                <p className="mb-2.5">Embracing new challenges with adaptability. Because nothing is too difficult when you're ready to grow.</p>
+                <div className="p-1.5 w-fit rounded-full bg-[#e6e6e6]">
+                    <button className="px-4 h-14 w-fit rounded-full bg-[#000000] shadow-2xs flex items-center gap-2 cursor-pointer">
+                        <div className="h-10 w-10 rounded-full bg-[#e6e6e6]">
+
                         </div>
-                        <span>
-                            Download
-                        </span>
+                        <p className="text-white">Download my CV</p>
                     </button>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
