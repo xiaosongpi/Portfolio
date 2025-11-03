eb1ec26215f71a4552c09b94f3fbd49991954325
xiaosongpi <minixiaopipi@gmail.com>
2025-11-03 21:38:25 +0700

    Adding light to hero section

--- my-app/src/app/components/Hero.jsx
+++ my-app/src/app/components/Hero.jsx
@@ -1,6 +1,9 @@
 export default function Hero() {
     return(
-        <section className="mx-auto h-screen max-w-[1440px] flex items-center justify-center">
+        <section className="mx-auto h-screen max-w-[1440px] flex items-center justify-center relative">
+            <div className="w-0 h-0 border-t-[100px] border-t-transparent border-b-[100px] border-b-transparent border-l-[1000px] border-l-[#ffffff] absolute top-0 left-[-500px] blur-[40px] -z-10 rotate-[60deg]"></div>
+            <div className="w-0 h-0 border-t-[100px] border-t-transparent border-b-[100px] border-b-transparent border-l-[800px] border-l-[#ffffff] absolute top-0 left-[-100px] blur-[40px] -z-10 rotate-[60deg]"></div>
+            <div className="w-0 h-0 border-t-[100px] border-t-transparent border-b-[100px] border-b-transparent border-l-[1200px] border-l-[#ffffff] absolute top-0 left-[100px] blur-[40px] -z-10 rotate-[60deg]"></div>
             <div className="w-[90%] flex flex-col items-center gap-6">
                 <div className="px-4 mb-2.5 h-14 w-fit border border-[#a5a5a5] rounded-full shadow-2xl flex items-center gap-2.5 bg-[#e6e6e6]">
                     <div className="h-2.5 w-2.5 rounded-full bg-green-500" />
